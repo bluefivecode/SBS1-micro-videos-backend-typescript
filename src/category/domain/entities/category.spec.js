3a6c1f27d850f93826058b4148b9596e2f533f2f
leofontolan <leofontolan@gmail.com>
2022-03-24 23:18:10 -0300

    Setters do Construtor de Category

---
+++ src/category/domain/entities/category.spec.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+describe('Category Tests', () => {
+    test('Constructor of Category', () => {
+    });
+});
--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -4,7 +4,6 @@ import { Category } from "./category";
 describe('Category Tests', () => {
     test('Constructor of Category', () => {
 
-        const category: Category = new Category('Movie');
-        expect(category.name).toBe('Movie');
+        
     })
 })
