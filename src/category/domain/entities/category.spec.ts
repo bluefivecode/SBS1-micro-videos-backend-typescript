254d9abf3070d810a0f84a8f895118b35ea1df62
leofontolan <leofontolan@gmail.com>
2022-05-28 14:13:41 -0300

    Criando teste de categoria

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,10 +1,21 @@
-
 import {Category} from './category';
 
-describe('Category Tests',()=>{
+describe('Category Unit Tests',()=>{
    
     test('constructor of category', ()=>{
-        const category: any = new Category('Movie');
+
+        const props = {
+            name: 'Movie',
+            description: 'some description',
+            is_active: true,
+            created_at: new Date()
+        }
+
+        const category: any = new Category(props);
+
         expect(category.name).toBe('Movie');
+        expect(category.description).toBe('some description');
+        expect(category.is_active).toBeTruthy();
+        expect(category.created_at).toBe(props.created_at);
     });
 });
