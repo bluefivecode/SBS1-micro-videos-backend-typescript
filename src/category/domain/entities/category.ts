92cd1950e554cc1c0f296b36e3dbfad5fd089756
leofontolan <leofontolan@gmail.com>
2022-05-28 13:57:51 -0300

    Criando getters em Category

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,8 +1,30 @@
+export type CategoryProperties = {
+    name: string;
+    description?: string;
+    is_active?: boolean;
+    created_at?: Date;
+
+}
+
 export class Category{
 
-    public name: string;S
+    constructor(public readonly pros: CategoryProperties){}
 
-    constructor(name: string){
-        this.name = name;
+    get name(): string{
+        return this.pros.name;
+    }
+
+    get description(): string | undefined{
+        return this.pros.description;
+    }
+
+    get is_active(): boolean | undefined{
+        return this.pros.is_active; 
+    }
+
+    get created_at(): Date | undefined{
+        return this.pros.created_at;
     }
 }
+
+const category = new Category({name:'', is_active:true, description:''});
