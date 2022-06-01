76a9715b98b62774ab7abc83bf041078a4d2dabe
leofontolan <leofontolan@gmail.com>
2022-06-01 16:49:49 -0300

    Testando Props de Category

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,8 +1,21 @@
 import { Category } from "./category";
 
 describe('Category Test', () => {
-    test('constructo of category', () => {
-        const category = new  Category('Movie');
-        expect(category.name).toBe('Movie');
+    test('constructor of category', () => {
+
+        const created_at = new Date();
+        const category = new Category({
+            name: 'Movie',
+            description: 'some description',
+            is_active: true,
+            created_at
+        });
+
+        expect(category.props).toMatchObject({
+            name: 'Movie',
+            description: 'some description',
+            is_active: true,
+            created_at
+        });
     });
 });
