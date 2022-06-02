9c7f0dc373745897e94061080bf028a0cd629b2f
leofontolan <leofontolan@gmail.com>
2022-06-02 13:42:40 -0300

    Teste created_at categoria

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -6,13 +6,11 @@ describe('Category Unit Test', () => {
 
        let category = new Category({name: 'Movie'});
        let props = omit(category.props, ['created_at']);
-
         expect(props).toStrictEqual({
             name: 'Movie',
             description: null,
             is_active: true,
         });
-
         expect(category.props.created_at).toBeInstanceOf(Date);
 
         category = new Category({
@@ -20,14 +18,42 @@ describe('Category Unit Test', () => {
             description: 'some description',
             is_active: false,
         });
-        
         let created_at = new Date();
-        
         expect(category.props).toStrictEqual({
             name: 'Movie',
             description: 'some description',
             is_active: false,
             created_at,
+        });
+
+        category = new Category({
+            name: 'Movie',
+            description: 'other description'
+        });
+        expect(category.props).toMatchObject({
+            name: 'Movie',
+            description: 'other description',
+        });
+
+        
+        category = new Category({
+            name: 'Movie',
+            is_active: true
+        });
+        expect(category.props).toMatchObject({
+            name: 'Movie',
+            is_active: true,
+        });
+
+
+        created_at = new Date();
+        category = new Category({
+            name: 'Movie',
+            created_at
+        });
+        expect(category.props).toMatchObject({
+            name: 'Movie',
+            created_at
         });
     });
 });
