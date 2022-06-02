8f7e770dff6bc2e3d6074184a075c028dcb4f58d
leofontolan <leofontolan@gmail.com>
2022-06-02 13:26:10 -0300

    Criando teste Category

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,21 +1,33 @@
 import { Category } from "./category";
+import { omit } from "lodash";
 
-describe('Category Test', () => {
+describe('Category Unit Test', () => {
     test('constructor of category', () => {
 
-        const created_at = new Date();
-        const category = new Category({
+       let category = new Category({name: 'Movie'});
+       let props = omit(category.props, ['created_at']);
+
+        expect(props).toStrictEqual({
             name: 'Movie',
-            description: 'some description',
+            description: null,
             is_active: true,
-            created_at,
         });
 
+        expect(category.props.created_at).toBeInstanceOf(Date);
+
+        category = new Category({
+            name: 'Movie', 
+            description: 'some description',
+            is_active: false,
+        });
+        
+        let created_at = new Date();
+        
         expect(category.props).toStrictEqual({
             name: 'Movie',
             description: 'some description',
-            is_active: true,
-            created_at
+            is_active: false,
+            created_at,
         });
     });
 });
